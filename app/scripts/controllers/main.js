b8d07ef2f4789c85762be4eff2432bc84e5a0977
Wader <1481234405@qq.com>
2017-05-12 11:39:43 +0800

    新增项目

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name wxWebApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the wxWebApp
+ */
+angular.module('wxWebApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
