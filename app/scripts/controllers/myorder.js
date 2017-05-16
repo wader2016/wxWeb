2bdf139f83f030c9141ee21e7a5360d3f7a819db
1481234405@qq.com <33a44b77d99c>
2017-05-16 22:45:57 +0800

    添加订单页

---
+++ app/scripts/controllers/myorder.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name wxWebApp.controller:MyorderCtrl
+ * @description
+ * # MyorderCtrl
+ * Controller of the wxWebApp
+ */
+angular.module('wxWebApp')
+  .controller('MyorderCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
